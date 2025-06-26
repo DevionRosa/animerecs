01f01275c02d1ba75662b7aa8af2084ac6b918d3
Devion Rosa <devionrosa@gmail.com>
2025-06-26 19:21:17 +0000

    flattened the dict and put it in sql

--- katsuapi.py
+++ katsuapi.py
@@ -1,9 +1,11 @@
 import requests
 import json
 import os
+import sqlalchemy as db
+import pandas as pd
+from pandas import json_normalize
 
 AUTH_URL = "https://kitsu.io/api/oauth/token"
-
 
 auth_response = requests.post(AUTH_URL, {
   'grant_type': 'password',
@@ -20,4 +22,18 @@ BASE_URL = "https://kitsu.io/api/edge/anime"
 anime_id = input("Input an anime ID: ")
 r = requests.get(BASE_URL + '/' + anime_id, headers = headers)
 
-print(r.json())
+anime_table = r.json()
+if 'data' in anime_table:
+  anime_attributes = anime_table['data'].get('attributes', {})
+  anime_id_val = anime_table['data'].get('id')
+  anime_df = json_normalize(anime_attributes)
+  anime_df['id'] = anime_id_val
+  for column in anime_df.columns:
+    if anime_df[column].apply(lambda x: isinstance(x, (dict, list))).any():
+      anime_df[column] = anime_df[column].astype(str)
+
+engine = db.create_engine('sqlite:///data_base_name.db')
+anime_df.to_sql('anime_df', con=engine, if_exists='replace', index=False)
+with engine.connect() as connection:
+  query_result = connection.execute(db.text("SELECT * FROM anime_df;")).fetchall()
+  print(pd.DataFrame(query_result, columns=anime_df.columns))
